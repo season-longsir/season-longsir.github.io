97731418ab01eafcc822701daeb3736d31e3e0db
season-longsir <123078051+season-longsir@users.noreply.github.com>
2024-08-08 12:55:40 +0800

    Update index.js

--- othergames/flappy/js/index.js
+++ othergames/flappy/js/index.js
@@ -472,7 +472,7 @@ function stageTooSmallHandler(){
         //game over logo
         context.drawImage(gameOverCanvas, bounds.getCenterX() - logoCanvas.width/2, canvas.height *.2);
 
-        var instruction = "Click or tap to flap again.";
+        var instruction = "点击屏幕开始游戏";
         context.font = "bold normal 24px sans-serif";
         context.fillStyle = "#FFFFFF";
         context.fillText(instruction, bounds.getCenterX() - context.measureText(instruction).width/2, canvas.height *.25 + gameOverCanvas.height);
@@ -490,7 +490,7 @@ function stageTooSmallHandler(){
     }
 
     function renderInstructions(){
-        var instruction = "Click or tap to flap :)";
+        var instruction = "点击屏幕开始游戏";
         context.font = "bold normal 24px sans-serif";
         context.fillStyle = "#FFFFFF";
         context.fillText(instruction, bounds.getCenterX() - context.measureText(instruction).width/2, canvas.height *.2);
